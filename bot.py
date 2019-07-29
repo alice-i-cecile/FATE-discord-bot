a6d367ec6e107c60e7e6395bec0f7c1fa81ab216
Rose Peck <olaf223224@gmail.com>
2019-07-29 13:07:40 -0400

    test commands, comments
    ok now stop pusing to master you doof

--- bot.py
+++ bot.py
@@ -4,18 +4,26 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-TOKEN = os.getenv('DISCORD_API_TOKEN')
+TOKEN = os.getenv('DISCORD_API_TOKEN') # bot token for the discord api
 bot = commands.Bot(command_prefix='!')
 
-@bot.event
+@bot.event # show yourself!
 async def on_ready():
     print('Logged in as')
     print(bot.user.name)
     print(bot.user.id)
     print('------')
 
-@bot.command()
+# simple testing command
+@bot.command() 
 async def ping(ctx):
     await ctx.send('pong!')
+    
+# slightly more complicated testing command
+@bot.command() 
+async def identity(ctx, *args):
+    await ctx.send('{} arguments: {}'.format(len(args), ', '.join(args)))
+
+
 
 bot.run(TOKEN)

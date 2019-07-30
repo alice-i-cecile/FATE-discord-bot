f108eed1c4cd7db065ea1b544b221448eccfda67
Rose Peck <olaf223224@gmail.com>
2019-07-30 11:20:27 -0400

    change new command to use groups, basic interactive prompt
    hoo rah! gonna do the rest of the prompts later i think

--- bot.py
+++ bot.py
@@ -3,9 +3,12 @@ from discord.ext import commands
 from dotenv import load_dotenv
 import os
 from models import *
+import utils
+import asyncio
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_API_TOKEN') # bot token for the discord api
+TIMEOUT = 10 # global timeout for bot prompts
 bot = commands.Bot(command_prefix='!')
 
 games = dict() # TODO: there's probably a better way to do this
@@ -27,17 +30,24 @@ async def ping(ctx):
 async def identity(ctx, *args):
     await ctx.send('{} arguments: {}'.format(len(args), ', '.join(args)))
 
-# command for creating new objects
-@bot.command(name='new', aliases=['n'])
-async def name(ctx, data, object):
-    if (data == "game"):
-        if (not (object in games)):
+# group for creating new objects
+@bot.group(name='new', aliases=['n'])
+async def new(ctx):
+    if ctx.invoked_subcommand is None:
+        await ctx.send('Please specify a type.')
+        
+# new game instance
+@new.command()
+async def game(ctx, object: str): # TODO proper error handling if object is not given (i think there's a nice way to do this)
+    if (not (object in games)): #make a new game babey!
+        await ctx.send('Creating a game named "{}"'.format(object))
+        await ctx.send('Would you like to use a ruleset template?')
+        try: # block to catch message timeouts
+            res = await bot.wait_for('message', check=utils.same_author_channel(ctx), timeout=TIMEOUT)
             games[object] = Game.create(object)
-            await ctx.send('Creating a game named "{}"'.format(object))
-            await ctx.send('Would you like to use a ruleset template?')
-            # TODO get a response from the user somehow...
-        else:
-            await ctx.send('That game name is already in use.')
-    # TODO: cases for "character" and "scene"
+        except asyncio.TimeoutError:
+            await ctx.send('Prompt timed out.')
+    else:
+        await ctx.send('That game name is already in use.')
             
 bot.run(TOKEN)
